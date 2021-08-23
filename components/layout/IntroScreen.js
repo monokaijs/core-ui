b723c07d0f4ae48b18c6802a4b8fd93c2a5d903f
MonokaiJs <monokaijs@gmail.com>
2021-08-23 17:32:04 +0700

    Fixed display of intro screen & updated elements

--- components/layout/IntroScreen.js
+++ components/layout/IntroScreen.js
@@ -99,7 +99,9 @@ const IntroScreen = (props) => {
 
   return show ? (
     <View style={{
-      flex: 1
+      flex: 1,
+      position: "absolute",
+      left: 0, right: 0, top: 0, bottom: 0
     }}>
       <Swiper horizontal={!props.vertical}>
         {props.screens.map((screen, index) => (
--- index.js
+++ index.js
@@ -2,9 +2,6 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 import Button from "./components/controls/Button";
 import Input from "./components/controls/Input";
 import Label from "./components/controls/Label";
@@ -13,6 +10,7 @@ import Navbar from "./components/layout/Navbar";
 import Scaffold from "./components/layout/Scaffold";
 import SplashScreen from "./components/layout/SplashScreen";
 import TabBar from "./components/layout/TabBar";
+import IntroScreen from "./components/layout/IntroScreen";
 
 export {
   Button,
@@ -22,7 +20,6 @@ export {
   Navbar,
   Scaffold,
   SplashScreen,
-  TabBar
+  TabBar,
+  IntroScreen
 }
-
-AppRegistry.registerComponent(appName, () => App);
